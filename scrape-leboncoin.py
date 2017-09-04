077eec870d5d2f2c97fccfd072b33a09fec8442b
Christophe Meyer <cmeyer@butterflynetinc.com>
2017-09-04 14:37:27 -0400

    Parallelize using threading and queue

--- scrape-leboncoin.py
+++ scrape-leboncoin.py
@@ -1,4 +1,6 @@
 import csv
+from queue import Queue
+import threading
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,6 +8,14 @@ from bs4 import BeautifulSoup
 
 URL_template = 'https://www.leboncoin.fr/locations/offres/ile_de_france/occasions/?o={}'
 max_page_number = 2000
+
+OUTPUT_CSV_FILE = 'rent.csv'
+HEADER = ['price', 'city', 'departement']
+
+# Serialize writing to the CSV file.
+# Multiple scape tasks put lines to write to the queue,
+# a single writer task gets lines from the queue and saves to CSV file.
+queue = Queue()
 
 
 def entries_for_page(page_number):
@@ -31,8 +41,8 @@ def extract_address(page_entry):
     return address
 
 
-def get_rent_infos():
-    for page_number in range(1, max_page_number):
+def get_rent_infos(page_numbers=range(1, max_page_number)):
+    for page_number in page_numbers:
 
         page_entries = entries_for_page(page_number)
         for page_entry in page_entries:
@@ -44,16 +54,40 @@ def get_rent_infos():
                 pass
 
 
-def main():
-    with open('rent.csv', 'w') as file:
-        file_writer = csv.writer(file)
-        file_writer.writerow(['price', 'city', 'departement'])
+def write_task():
+    def do_work(file_writer, file, row):
+        print('writing', row)
+        file_writer.writerow(row)
+        file.flush()
 
-        for rent_info in get_rent_infos():
-            price = rent_info['price']
-            address = rent_info['address']
-            print(price, address)
-            file_writer.writerow([price, address['city'], address['departement']])
+    with open(OUTPUT_CSV_FILE, 'w') as file:
+        file_writer = csv.writer(file)
+        do_work(file_writer, file, HEADER)
+        while True:
+            item = queue.get()
+            do_work(file_writer, file, item)
+            queue.task_done()
+
+
+def scrape_task(page_numbers):
+    for rent_info in get_rent_infos(page_numbers):
+        price = rent_info['price']
+        address = rent_info['address']
+        row = [price, address['city'], address['departement']]
+        queue.put(row)
+
+
+def main():
+    num_worker_threads = 8
+    num_pages_per_thread = max_page_number // num_worker_threads
+    for i_thread in range(num_worker_threads):
+        page_numbers = range(i_thread*num_pages_per_thread,
+                             (i_thread+1) * num_pages_per_thread)
+        scrape_thread = threading.Thread(target=scrape_task, args=(page_numbers,))
+        scrape_thread.start()
+
+    write_thread = threading.Thread(target=write_task)
+    write_thread.start()
 
 
 if __name__ == '__main__':
